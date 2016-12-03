3ff7505157dd11dba7fbc3f4d95829ed7b523466
Tristan Partin <tristan.partin@gmail.com>
2016-12-03 14:46:46 -0600

    Initial creation of a GUI window
    Created the first attempt at a GTK window including a headerbar and file button (needs a title for the button).

---
+++ mainWindow.cxx
@@ -0,0 +1,42 @@
+#include <gtk/gtk.h>
+
+static void activate(GtkApplication* app, gpointer user_data)
+{
+	GtkWidget* mainWindow;
+	GtkWidget* header;
+	GtkWidget* fileButton;
+	GIcon* fileIcon;
+	GtkWidget* fileImage;
+
+	mainWindow = gtk_application_window_new(app);
+	//gtk_window_set_title(GTK_WINDOW(mainWindow), "Stop Bitchin' - Start Watchin'");
+	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 1200, 720);
+
+	header = gtk_header_bar_new();
+	gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
+	gtk_header_bar_set_title(GTK_HEADER_BAR(header), "Stop Bitchin' - Start Watchin'");
+	gtk_header_bar_set_has_subtitle(GTK_HEADER_BAR(header), FALSE);
+	gtk_window_set_titlebar(GTK_WINDOW(mainWindow), header);
+	fileButton = gtk_file_chooser_button_new("Choose a file", GTK_FILE_CHOOSER_ACTION_OPEN);
+
+	fileIcon = g_themed_icon_new("mail-send-receive-symbolic");
+	fileImage = gtk_image_new_from_gicon(fileIcon, GTK_ICON_SIZE_BUTTON);
+	g_object_unref(fileIcon);
+	gtk_container_add(GTK_CONTAINER(fileButton), fileImage);
+	gtk_header_bar_pack_start(GTK_HEADER_BAR(header), fileButton);
+
+	gtk_widget_show_all(mainWindow);
+}
+
+int main (int argc, char** argv)
+{
+  GtkApplication* app;
+  int status;
+
+  app = gtk_application_new ("com.github.tristan957.stop_bitchin-start_watchin", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
+}
