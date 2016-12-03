ff7b3c107c639b7e67a778dce7376b10036db7e2
Joseph Martinsen <joseph@martinsen.com>
2016-12-03 15:47:48 -0600

    Incorrect Parsing of CSV

--- readFile.hxx
+++ readFile.hxx
@@ -1,18 +1,39 @@
-#ifndef READ_FILE_HXX
-#define READ_FILE_HXX
-
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
-#include <string>
+#include "readFile.hxx"
 #include <vector>
-#include "Movie.hxx"
 
 
-using namespace std;
+void readFile(string file_name)
+{
+  ifstream file (file_name);
+  if(!file) throw runtime_error("Failed to open file " + file_name);
+  tring value;
+  string value;
+  vector<string> test;
 
-void readFile(string file_name);
-// string input_file_name();
-// vector<vector<string>> read(string file_name);
 
-#endif
+  while ( file.good() )
+  {
+     getline ( file, value, ',' ); // read a string until next comma:
+      // cout << string( value, 0, value.length() ) << endl;
+
+    // If cell has multible values
+    if (value[0] == '"' )
+    {
+      int startVal = 1;
+      string temp = value;
+      while (temp[0] = ' ')
+      {
+        temp = value.substr(startVal,value.find(","));
+        test.push_back(temp);
+        std::cout << value;
+        startVal = value.find(",") + 1;
+      }
+      for (int i = 0; i < test.size(); i++) {
+        std::cout << test.at(i) << '\n';
+      }
+    } else
+    {
+      cout <<  value << endl;
+    }
+  }
+} 
