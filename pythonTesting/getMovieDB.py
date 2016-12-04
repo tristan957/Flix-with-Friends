def84154d57bf7bc813917b97dbc7df7c1405a3a
Joseph Martinsen <joseph@martinsen.com>
2016-12-03 20:47:38 -0600

    Comment and Such

--- pythonTesting/getMovieDB.py
+++ pythonTesting/getMovieDB.py
@@ -1,33 +1,37 @@
 import tmdbsimple as tmdb
 tmdb.API_KEY = 'b299f0e8dce095f8ebcbae6ab789005c'
-
+# Initialize the search for The movie database
 search = tmdb.Search()
 
+# Open the CSV file and read the lines
 with open("Movies.csv") as f:
     content = f.readlines()
 
-j = 0
+# Func for determining lines in the CSV file
+
+
 def file_len(fname):
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
 
+
+# Utilize the function
 length = file_len("Movies.csv")
 
+# For all the movie titles before the delimitter, run a search and
+#   and use the first earch result's  movie ID to get info
 for i in range(length):
     a = content[i].split(",")
     print('CORRECT MOVIE: ', a[0])
-
-    # Search for info about Forest Gump
 
     response = search.movie(query=a[0])
 
     i = 0
     for s in search.results:
-        i = 1+i
+        i = 1 + i
         if i == 1:
-            j = j+1
             # titleMovie = s['title']
             titleID = s['id']
             # print(titleMovie, titleID)
@@ -35,27 +39,4 @@ for i in range(length):
             response = forrestGump.info()
             print(response['title'])
             print('Runtime: ', response['runtime'])
-            print('Overview: ', response['overview'])
-            print(j)
-
-            print('\n')
-
-
-# 
-# # Forrest Gump
-# forrestGump = tmdb.Movies(13)
-# response = forrestGump.info()
-# print(response['title'])
-#
-# # Saving Private Ryan
-# savingPrivateRyan = tmdb.Movies(857)
-# savingPrivateRyanInfo = savingPrivateRyan.info()
-# print(savingPrivateRyanInfo['title'])
-#
-# # Dead Poet Society
-#
-# # Mulan
-#
-# # Shrek
-#
-# # P.S. I love You
+            print('Overview: ', response['overview'], '\n')
