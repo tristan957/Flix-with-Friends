b38056f0ce1c0a459eea5fac8af54d942f24bc95
Joseph Martinsen <joseph@martinsen.com>
2016-12-03 20:14:56 -0600

    API fully functional

--- pythonTesting/getMovieDB.py
+++ pythonTesting/getMovieDB.py
@@ -1,20 +1,61 @@
 import tmdbsimple as tmdb
 tmdb.API_KEY = 'b299f0e8dce095f8ebcbae6ab789005c'
 
-# Forrest Gump
-forrestGump = tmdb.Movies(13)
-response = forrestGump.info()
-print(response['title'])
+search = tmdb.Search()
 
-# Saving Private Ryan
-savingPrivateRyan = tmdb.Movies(857)
-savingPrivateRyanInfo = savingPrivateRyan.info()
-print(savingPrivateRyanInfo['title'])
+with open("Movies.csv") as f:
+    content = f.readlines()
 
-# Dead Poet Society
+j = 0
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
-# Mulan
+length = file_len("Movies.csv")
 
-# Shrek
+for i in range(length):
+    a = content[i].split(",")
+    print('CORRECT MOVIE: ', a[0])
 
-# P.S. I love You
+    # Search for info about Forest Gump
+
+    response = search.movie(query=a[0])
+
+    i = 0
+    for s in search.results:
+        i = 1+i
+        if i == 1:
+            j = j+1
+            # titleMovie = s['title']
+            titleID = s['id']
+            # print(titleMovie, titleID)
+            forrestGump = tmdb.Movies(titleID)
+            response = forrestGump.info()
+            print(response['title'])
+            print('Runtime: ', response['runtime'])
+            print('Overview: ', response['overview'])
+            print(j)
+
+            print('\n')
+
+
+# 
+# # Forrest Gump
+# forrestGump = tmdb.Movies(13)
+# response = forrestGump.info()
+# print(response['title'])
+#
+# # Saving Private Ryan
+# savingPrivateRyan = tmdb.Movies(857)
+# savingPrivateRyanInfo = savingPrivateRyan.info()
+# print(savingPrivateRyanInfo['title'])
+#
+# # Dead Poet Society
+#
+# # Mulan
+#
+# # Shrek
+#
+# # P.S. I love You
