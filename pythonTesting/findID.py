ffe7cb4ef90374bbec1d9629161d17cefd2e5c43
Joseph Martinsen <joseph@martinsen.com>
2016-12-03 19:22:27 -0600

    findID outputs to console

--- pythonTesting/findID.py
+++ pythonTesting/findID.py
@@ -1,6 +1,8 @@
 import tmdbsimple as tmdb
 tmdb.API_KEY = 'b299f0e8dce095f8ebcbae6ab789005c'
 
+search = tmdb.Search()
+
 with open("Movies.csv") as f:
     content = f.readlines()
 
@@ -14,18 +16,27 @@ length = file_len("Movies.csv")
 
 for i in range(length):
     a = content[i].split(",")
-    print(a[0])
+    print('CORRECT MOVIE NAME: ', a[0])
 
-search = tmdb.Search()
+    # Search for info about Forest Gump
+    response = search.movie(query=a[0])
+
+    for s in search.results:
+        print (s['title'])
+        print(s['id'])
+        # print( s['overview'])
+        print('\n')
+
+
 # response = search.movie(query='259316')
 
 
 
-# Search for info about Forest Gump
-response = search.movie(query='Saving Private Ryan')
-
-for s in search.results:
-    print (s['title'])
-    print(s['id'])
-    # print( s['overview'])
-    print('\n')
+# # Search for info about Forest Gump
+# response = search.movie(query='Saving Private Ryan')
+#
+# for s in search.results:
+#     print (s['title'])
+#     print(s['id'])
+#     # print( s['overview'])
+#     print('\n')
