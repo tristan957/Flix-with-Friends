42127c723d96a193f4ab6a6c6a5c78b2f9aa2e03
Joseph Martinsen <joseph@martinsen.com>
2016-12-03 21:06:57 -0600

    Can use getMovieDB from Console

--- pythonTesting/getMovieDB.py
+++ pythonTesting/getMovieDB.py
@@ -1,3 +1,4 @@
+import sys
 import tmdbsimple as tmdb
 tmdb.API_KEY = 'b299f0e8dce095f8ebcbae6ab789005c'
 # Initialize the search for The movie database
@@ -17,16 +18,13 @@ def file_len(fname):
     return i + 1
 
 
-# Utilize the function
-length = file_len("Movies.csv")
 
-# For all the movie titles before the delimitter, run a search and
-#   and use the first earch result's  movie ID to get info
-for i in range(length):
-    a = content[i].split(",")
-    print('CORRECT MOVIE: ', a[0])
 
-    response = search.movie(query=a[0])
+if len(sys.argv) > 1:
+    arguments = sys.argv
+    a = " ".join(arguments[1:])
+    # print(a)
+    response = search.movie(query=a)
 
     i = 0
     for s in search.results:
@@ -40,3 +38,29 @@ for i in range(length):
             print(response['title'])
             print('Runtime: ', response['runtime'])
             print('Overview: ', response['overview'], '\n')
+
+else:
+
+    # Utilize the function
+    length = file_len("Movies.csv")
+
+    # For all the movie titles before the delimitter, run a search and
+    #   and use the first earch result's  movie ID to get info
+    for i in range(length):
+        a = content[i].split(",")
+        print('CORRECT MOVIE: ', a[0])
+
+        response = search.movie(query=a[0])
+
+        i = 0
+        for s in search.results:
+            i = 1 + i
+            if i == 1:
+                # titleMovie = s['title']
+                titleID = s['id']
+                # print(titleMovie, titleID)
+                forrestGump = tmdb.Movies(titleID)
+                response = forrestGump.info()
+                print(response['title'])
+                print('Runtime: ', response['runtime'])
+                print('Overview: ', response['overview'], '\n')
