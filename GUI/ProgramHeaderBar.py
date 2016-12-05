fb83298046756f68ae00d4e524a9e15b95aaef18
Tristan Partin <tristan.partin@gmail.com>
2016-12-04 22:05:02 -0600

    Created a search button
    Created a button with a magnifing glass Icon. Icon is dark right now but I will look for help on forums and online. Also instantiated a class for a SearchBar which the searchButton will eventually correspond to.

--- GUI/ProgramHeaderBar.py
+++ GUI/ProgramHeaderBar.py
@@ -1,6 +1,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from gi.repository import Gio
 
 class ProgramHeaderBar(Gtk.HeaderBar):
 
@@ -9,8 +10,17 @@ class ProgramHeaderBar(Gtk.HeaderBar):
 
 		self.fileButton = Gtk.FileChooserButton()	#create a Gtk.FileChooserButton
 		self.fileButton.connect("file-set", self.fileButton_cb)	#connects the file-set signal to fileButton_cb
-		self.pack_start(self.fileButton)
+		self.pack_start(self.fileButton)	#adds the button to the start of the headerbar
+
+		self.searchImage = Gtk.Image(stock = Gtk.STOCK_FIND)	#create an image to place on the button
+		self.searchButton = Gtk.Button(image = self.searchImage)	#creates a button with an image
+		self.searchButton.connect("activate", self.searchButton_cb)	#connects the activate signal to searchButton_cb
+		self.pack_end(self.searchButton)	#adds the button to the end of the headerbar
 
 	#callback for when the fileButton is pressed
 	def fileButton_cb(self, fileButton):
 		filename = fileButton.get_filename()
+
+	#callback for when the searchButton is pressed
+	def searchButton_cb(self, searchButton):
+		print("search")
