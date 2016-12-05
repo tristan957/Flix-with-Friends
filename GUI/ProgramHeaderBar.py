cee90d6f4ac1b1bcbab6f609d484b23a6dbbb4bf
Tristan Partin <tristan.partin@gmail.com>
2016-12-05 01:14:57 -0600

    Added a random movie button
    Added a random movie button in the headerbar. Still fucking around with this search bar dealio.

--- GUI/ProgramHeaderBar.py
+++ GUI/ProgramHeaderBar.py
@@ -12,15 +12,22 @@ class ProgramHeaderBar(Gtk.HeaderBar):
 		self.fileButton.connect("file-set", self.fileButton_cb)	#connects the file-set signal to fileButton_cb
 		self.pack_start(self.fileButton)	#adds the button to the start of the headerbar
 
+		self.randomMovieButton = Gtk.Button(label = "Random Movie")
+		self.randomMovieButton.connect("clicked", self.randomMovieButton_cb)
+		self.pack_end(self.randomMovieButton)
+
 		self.searchImage = Gtk.Image(stock = Gtk.STOCK_FIND)	#create an image to place on the button
 		self.searchButton = Gtk.Button(image = self.searchImage)	#creates a button with an image
-		self.searchButton.connect("activate", self.searchButton_cb)	#connects the activate signal to searchButton_cb
+		self.searchButton.connect("clicked", self.searchButton_cb)	#connects the activate signal to searchButton_cb
 		self.pack_end(self.searchButton)	#adds the button to the end of the headerbar
 
 	#callback for when the fileButton is pressed
 	def fileButton_cb(self, fileButton):
 		filename = fileButton.get_filename()
 
+	def randomMovieButton_cb(self, randomMovieButton):
+		print("Random Movie")
+
 	#callback for when the searchButton is pressed
 	def searchButton_cb(self, searchButton):
-		print("search")
+		print("Search")
--- GUI/SearchBar.py
+++ GUI/SearchBar.py
@@ -2,4 +2,10 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class SearchBar(Gtk.Container)
+class SearchBar(Gtk.Box):
+	#needs a grid of GtkToggleButtons
+	def __init__(self, win):
+		Gtk.Box.__init__(Gtk.Orientation.VERTICAL, spacing = 100)
+
+		#self.entry = Gtk.SearchEntry()
+		#self.set_halign(Gtk.Align.START)
