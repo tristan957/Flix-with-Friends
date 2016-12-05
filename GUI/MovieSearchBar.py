1092b5d6640ea030e10ca60b127a85a6e5eefbfd
Tristan Partin <tristan.partin@gmail.com>
2016-12-05 15:27:14 -0600

    Added functionality to search categories
    When a category is toggled , the label is added to an array. Upon being toggled again, the label is removed from the array. A getCategories function has also been implemented.

--- GUI/MovieSearchBar.py
+++ GUI/MovieSearchBar.py
@@ -7,6 +7,8 @@ class MovieSearchBar(Gtk.Box):
 	def __init__(self):
 		Gtk.Box.__init__(self, orientation = Gtk.Orientation.HORIZONTAL, spacing = 50)
 
+		self.categories = []
+
 		self.search = Gtk.SearchBar(search_mode_enabled = True, show_close_button = True)
 		self.entry = Gtk.SearchEntry()
 		self.search.connect_entry(self.entry)
@@ -25,4 +27,20 @@ class MovieSearchBar(Gtk.Box):
 		self.buttonBox.pack_start(self.genreButton, True, True, 0)
 		self.buttonBox.pack_start(self.yearButton, True, True, 0)
 		self.buttonBox.pack_start(self.unviewedButton, True, True, 0)
+
+		self.nameButton.connect("toggled", self.searchCategories_cb)
+		self.descriptionButton.connect("toggled", self.searchCategories_cb)
+		self.genreButton.connect("toggled", self.searchCategories_cb)
+		self.yearButton.connect("toggled", self.searchCategories_cb)
+		self.unviewedButton.connect("toggled", self.searchCategories_cb)
+
 		self.pack_start(self.buttonBox, True, True, 0)
+
+	def searchCategories_cb(self, searchButton):
+		if searchButton.get_active() == True:
+			self.categories.append(searchButton.get_label())
+		else:
+			self.categories.remove(searchButton.get_label())
+
+	def getCategories(self):
+		return self.categories
