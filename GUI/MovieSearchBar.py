eb7e6f2b27ba3e76b687026e1951153096262dec
Tristan Partin <tristan.partin@gmail.com>
2016-12-16 00:18:34 -0600

    Connect "search-changed" signal for entry
    Implemented this so that we can update search results live instead of waiting for a whole keyword.

--- GUI/MovieSearchBar.py
+++ GUI/MovieSearchBar.py
@@ -6,58 +6,59 @@ from gi.repository import Gtk
 class MovieSearchBar(Gtk.Box):
     # needs a grid of GtkToggleButtons
 
-    def __init__(self):
-        Gtk.Box.__init__(
-            self, orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+	def __init__(self):
+		Gtk.Box.__init__(self, orientation = Gtk.Orientation.HORIZONTAL, spacing = 50)
 
-        self.categories = []
+		self.categories = []
 
-        self.search = Gtk.SearchBar(search_mode_enabled=True, show_close_button=True)
-        self.entry = Gtk.SearchEntry()
-        self.search.connect_entry(self.entry)
-        self.entry.grab_focus()
-        self.pack_start(self.entry, True, True, 0)
+		self.search = Gtk.SearchBar(search_mode_enabled = True, show_close_button = True)
+		self.entry = Gtk.SearchEntry()
+		self.search.connect_entry(self.entry)
+		self.entry.grab_focus()
+		self.pack_start(self.entry, True, True, 0)
 
-        # Callback for when enter key is pressed
-        self.entry.connect("activate", self.search_cb)
+		# Callback for when enter key is pressed
+		self.entry.connect("activate", self.search_cb)
+		self.entry.connect("search-changed", self.search_changed_cb)
 
-        self.nameButton = Gtk.ToggleButton(label="Name")
-        self.descriptionButton = Gtk.ToggleButton(label="Description")
-        self.genreButton = Gtk.ToggleButton(label="Genre")
-        self.dateButton = Gtk.ToggleButton(label="Release Date")
-        self.viewedByButton = Gtk.ToggleButton(label="Viewed By")
-        self.ratingButton = Gtk.ToggleButton(label="Rating")
+		self.nameButton = Gtk.ToggleButton(label = "Name")
+		self.descriptionButton = Gtk.ToggleButton(label = "Description")
+		self.genreButton = Gtk.ToggleButton(label = "Genre")
+		self.dateButton = Gtk.ToggleButton(label = "Release Date")
+		self.viewedByButton = Gtk.ToggleButton(label = "Viewed By")
+		self.ratingButton = Gtk.ToggleButton(label = "Rating")
 
-        self.buttonBox = Gtk.Box(
-            orientation=Gtk.Orientation.HORIZONTAL, spacing=2)
-        self.buttonBox.pack_start(self.nameButton, True, True, 0)
-        self.buttonBox.pack_start(self.descriptionButton, True, True, 0)
-        self.buttonBox.pack_start(self.genreButton, True, True, 0)
-        self.buttonBox.pack_start(self.dateButton, True, True, 0)
-        self.buttonBox.pack_start(self.viewedByButton, True, True, 0)
-        self.buttonBox.pack_start(self.ratingButton, True, True, 0)
+		self.buttonBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 2)
+		self.buttonBox.pack_start(self.nameButton, True, True, 0)
+		self.buttonBox.pack_start(self.descriptionButton, True, True, 0)
+		self.buttonBox.pack_start(self.genreButton, True, True, 0)
+		self.buttonBox.pack_start(self.dateButton, True, True, 0)
+		self.buttonBox.pack_start(self.viewedByButton, True, True, 0)
+		self.buttonBox.pack_start(self.ratingButton, True, True, 0)
 
-        self.nameButton.connect("toggled", self.searchCategories_cb)
-        self.descriptionButton.connect("toggled", self.searchCategories_cb)
-        self.genreButton.connect("toggled", self.searchCategories_cb)
-        self.dateButton.connect("toggled", self.searchCategories_cb)
-        self.viewedByButton.connect("toggled", self.searchCategories_cb)
-        self.ratingButton.connect("toggled", self.searchCategories_cb)
+		self.nameButton.connect("toggled", self.searchCategories_cb)
+		self.descriptionButton.connect("toggled", self.searchCategories_cb)
+		self.genreButton.connect("toggled", self.searchCategories_cb)
+		self.dateButton.connect("toggled", self.searchCategories_cb)
+		self.viewedByButton.connect("toggled", self.searchCategories_cb)
+		self.ratingButton.connect("toggled", self.searchCategories_cb)
 
-        self.pack_start(self.buttonBox, True, True, 0)
+		self.pack_start(self.buttonBox, True, True, 0)
 
-    def searchCategories_cb(self, searchButton):
-        if searchButton.get_active() == True:
-            self.categories.append(searchButton.get_label())
-            print(self.categories)
+	def searchCategories_cb(self, searchButton):
+		if searchButton.get_active() == True:
+			self.categories.append(searchButton.get_label())
+			print(self.categories)
+		else:
+			self.categories.remove(searchButton.get_label())
+			print(self.categories)
 
-        else:
-            self.categories.remove(searchButton.get_label())
-            print(self.categories)
+	def getCategories(self):
+		return self.categories
 
-    def getCategories(self):
-        return self.categories
+	def search_cb(self, entry):
+		# retrieve the content of the widget
+		print(entry.get_text())
 
-    def search_cb(self, entry):
-        # retrieve the content of the widget
-        print(entry.get_text())
+	def search_changed_cb(self, entry):
+		print(entry.get_text())
--- GUI/MovieWindow.py
+++ GUI/MovieWindow.py
@@ -11,5 +11,9 @@ class MovieWindow(Gtk.Window):
 
 		header = MovieHeaderBar()	#create headerbar
 		self.set_titlebar(header)	#add it to the window
-		self.search = MovieSearchBar()
-		self.add(self.search)
+		self.searchBar = MovieSearchBar()
+		self.add(self.searchBar)
+		self.connect("key-press-event", self.key_pressed_cb)
+
+	def key_pressed_cb(self, win, event):
+		return self.searchBar.search.handle_event(event)
