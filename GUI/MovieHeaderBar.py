469f90e3614a89f2598674b7d29e7e8f11268179
Tristan Partin <tristan.partin@gmail.com>
2016-12-22 20:46:33 -0600

    Added focus to the search bar entry
    Moved focus to the search bar entry if a key is pressed or the search button is pressed

--- GUI/MovieHeaderBar.py
+++ GUI/MovieHeaderBar.py
@@ -5,7 +5,7 @@ from MovieDialog import MovieDialog
 
 class MovieHeaderBar(Gtk.HeaderBar):
 
-	def __init__(self, reveal):
+	def __init__(self, reveal, searchBar):
 		Gtk.HeaderBar.__init__(self, title = "Stop Bitchin', Start Watchin'", show_close_button = True)
 
 		self.fileButton = Gtk.FileChooserButton()	#create a Gtk.FileChooserButton
@@ -36,7 +36,7 @@ class MovieHeaderBar(Gtk.HeaderBar):
 		self.searchIcon = Gio.ThemedIcon(name = "edit-find-symbolic")	#create an image to place on the button
 		self.searchImage = Gtk.Image.new_from_gicon(self.searchIcon, Gtk.IconSize.BUTTON)
 		self.searchButton = Gtk.ToggleButton(image = self.searchImage)	#creates a button with an image
-		self.searchButton.connect("clicked", self.searchButton_cb, reveal)	#connects the activate signal to searchButton_cb
+		self.searchButton.connect("clicked", self.searchButton_cb, reveal, searchBar)	#connects the activate signal to searchButton_cb
 		self.pack_end(self.searchButton)	#adds the button to the end of the headerbar
 
 	#callback for when the fileButton is pressed
@@ -47,13 +47,15 @@ class MovieHeaderBar(Gtk.HeaderBar):
 		print("Random Movie")
 
 	#callback for when the searchButton is pressed
-	def searchButton_cb(self, searchButton, reveal):
+	def searchButton_cb(self, searchButton, reveal, searchBar):
 		if searchButton.get_active() == True:
 			reveal.set_transition_type(Gtk.RevealerTransitionType.SLIDE_DOWN)
 			reveal.set_reveal_child(True)
+			searchBar.entry.grab_focus()
 		else:
 			reveal.set_transition_type(Gtk.RevealerTransitionType.SLIDE_UP)
 			reveal.set_reveal_child(False)
+			self.grab_focus()
 
 	def dataButton_cb(self, dataButton):
 		self.dataPopover.show_all()
--- GUI/MovieWindow.py
+++ GUI/MovieWindow.py
@@ -12,12 +12,13 @@ class MovieWindow(Gtk.Window):
 		self.searchBar = MovieSearchBar()
 		self.reveal = Gtk.Revealer(child = self.searchBar, transition_duration = 500)
 		self.add(self.reveal)
-		self.header = MovieHeaderBar(self.reveal)	#create headerbar
+		self.header = MovieHeaderBar(self.reveal, self.searchBar)	#create headerbar
 		self.set_titlebar(self.header)	#add it to the window
-		self.connect("key-press-event", self.key_pressed_cb, self.reveal, self.header)
+		self.connect("key-press-event", self.key_pressed_cb, self.reveal, self.searchBar, self.header)
 
-	def key_pressed_cb(self, win, event, reveal, header):
+	def key_pressed_cb(self, win, event, reveal, searchBar, header):
 		reveal.set_transition_type(Gtk.RevealerTransitionType.SLIDE_DOWN)
 		header.searchButton.set_active(True)
 		reveal.set_reveal_child(True)
+		searchBar.entry.grab_focus()
 		return self.searchBar.search.handle_event(event)
