394bee22c17ce8bc07c9fe509f81aef7aca97230
Joseph Martinsen <joseph@martinsen.com>
2017-01-02 22:33:14 -0600

    Change poster size to w342

--- MovieBox.py
+++ MovieBox.py
@@ -12,7 +12,7 @@ class MovieBox(Gtk.Box):
         self.infoBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
 
         self.titleLabel = Gtk.Label(label = "<big>Iron Man</big>", justify = Gtk.Justification.CENTER, use_markup = True)
-        self.poster = Gtk.Image(file = "./imagePosters/SavingPrivateRyan_w154.jpg")
+        self.poster = Gtk.Image(file = "./imagePosters/SavingPrivateRyan_w342.jpg")
         self.viewedLabel = Gtk.Label(label = "Viewed By: T, C, G, A, M, P", justify = Gtk.Justification.LEFT)
         self.viewedLabel.set_xalign(0)
         self.ratingLabel = Gtk.Label(label = "Rating: 8/10", justify = Gtk.Justification.LEFT)
