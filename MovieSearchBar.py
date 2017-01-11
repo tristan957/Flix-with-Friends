53d6cee8f92593f236a49908e353404db23c79f3
Joseph Martinsen <joseph@martinsen.com>
2017-01-10 22:30:17 -0600

    Revert "No longer poop"
    This reverts commit ba8b186f18ccc2e4ce152929d38c9312dd6ef1aa.

--- MovieSearchBar.py
+++ MovieSearchBar.py
@@ -56,11 +56,7 @@ class MovieSearchBar(Gtk.Box):
 		self.viewedByPopover.add(self.viewedByBox)
 
 		self.ratingPopover = Gtk.Popover()
-
-		ad1 = Gtk.Adjustment(0, 0, 255, 1, 20, 0)
-		self.scale = Gtk.Scale(orientation=Gtk.Orientation.VERTICAL, adjustment=ad1)
-		self.scale.set_size_request(50, 350)
-		# self.scale = Gtk.Scale(draw_value = True).new_with_range(Gtk.Orientation.VERTICAL, 0, 10, 1)
+		self.scale = Gtk.Scale(draw_value = True).new_with_range(Gtk.Orientation.VERTICAL, 0, 10, 1)
 		self.ratingPopover.add(self.scale)
 
 		self.genreButton = Gtk.MenuButton(label = "Genre", use_popover = True, popover = self.genrePopover)
