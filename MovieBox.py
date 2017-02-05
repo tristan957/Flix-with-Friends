fd9642798b379331c07218d62459ac04ebbde1b6
Joseph Martinsen <joseph@martinsen.com>
2017-02-05 01:17:44 -0600

    Viewers and Genres added to MovieBox

--- MovieBox.py
+++ MovieBox.py
@@ -10,7 +10,10 @@ class MovieBox(Gtk.Box):
 
     def __init__(self):
         db = Database(Database.location)
-        movie = db.movies[MOVIE_INDEX]
+        movie = db.movies[10]
+
+
+
         Gtk.Box.__init__(self, orientation = Gtk.Orientation.HORIZONTAL)
 
         self.imageBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 30)
@@ -18,8 +21,10 @@ class MovieBox(Gtk.Box):
 
         self.titleLabel = Gtk.Label(label = "<big><b>" + movie.title + "</b></big>", justify = Gtk.Justification.CENTER, use_markup = True)
         self.poster = Gtk.Image(file = "./imagePosters/" + movie.title.replace(" ", "") + "_w342.jpg")
-        self.viewedLabel = Gtk.Label(label = "<b>Viewed By:</b> T, C, G, A, M, P", justify = Gtk.Justification.LEFT, use_markup = True)
+        self.viewedLabel = Gtk.Label(label = "<b>Viewed By:</b> " + ' '.join(movie.viewers), justify = Gtk.Justification.LEFT, use_markup = True)
         self.viewedLabel.set_xalign(0)
+        self.genreLabel = Gtk.Label(label = "<b>Genres:</b> " + ' '.join(movie.genres), justify = Gtk.Justification.LEFT, use_markup = True)
+        self.genreLabel.set_xalign(0)
         self.ratingLabel = Gtk.Label(label = "<b>Rating:</b> " + str(movie.vote) + "/10", justify = Gtk.Justification.LEFT, use_markup = True)
         self.ratingLabel.set_xalign(0)
         self.runtimeLabel = Gtk.Label(label = "<b>Runtime:</b> " + str(int(movie.runtime) // 60) + " Hours " + str(int(movie.runtime) % 60) + " Minutes", justify = Gtk.Justification.LEFT, use_markup = True)
@@ -32,6 +37,7 @@ class MovieBox(Gtk.Box):
         self.imageBox.add(self.poster)
 
         self.infoBox.add(self.viewedLabel)
+        self.infoBox.add(self.genreLabel)
         self.infoBox.add(self.ratingLabel)
         self.infoBox.add(self.runtimeLabel)
         self.infoBox.add(self.description)
