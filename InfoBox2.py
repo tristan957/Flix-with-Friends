fa9e77eccdf9bbcdb52d1f29172e3554516a5a66
Tristan Partin <tristan.partin@gmail.com>
2017-03-22 21:18:23 -0500

    Frame classes finished

--- InfoBox2.py
+++ InfoBox2.py
@@ -31,12 +31,42 @@ class GenreFrame(Gtk.Frame):
     def __init__(self, movie):
         Gtk.Frame(self, label_xalign = .1)
 
+        title = Gtk.Label(label = "<b>Genres</b>", use_markup = True)
+        self.set_label_widget(title)
+
+		self.genres = Gtk.Label(label = movie.get_genres_string(), wrap = True)
+
+		box = Gtk.Box(margin_bottom = 5, margin_left = 3, margin_right = 3)
+		box.add(self.genres)
+
+        self.add(genreBox)
+
+    def update(self, movie):
+        self.genres.set_label(movie.get_genres_string())
+
 
 class RatingFrame(Gtk.Frame):
     """Frame for displaying the rating of the movie"""
 
     def __init__(self, movie):
         Gtk.Frame(self, label_xalign = .1)
+
+        title = Gtk.Label(label = "<b>Rating</b>", use_markup = True)
+        self.set_label_widget(title)
+
+        self.rating = Gtk.Label(label = movie.vote + "/10", wrap = True)
+		self.level = Gtk.LevelBar(value = float(self.movie.vote)).new_for_interval(0, 10)
+		self.level.set_size_request(100, -1)
+
+		box = Gtk.Box(margin_bottom = 5, margin_left = 3, margin_right = 3, spacing = 5)
+		box.add(self.level)
+	    box.add(self.rating)
+
+		self.add(ratingBox)
+
+    def update(self, movie):
+        self.rating.set_label(movie.vote)
+        self.level.set_value(float(movie.vote))
 
 
 class RuntimeFrame(Gtk.Frame):
@@ -45,6 +75,19 @@ class RuntimeFrame(Gtk.Frame):
     def __init__(self, movie):
         Gtk.Frame(self, label_xalign = .1)
 
+        title = Gtk.Label(label = "<b>Runtime</b>", use_markup = True)
+        self.set_label_widget(title)
+		self.runtime = Gtk.Label(label = str(int(movie.runtime) // 60) + " Hours " +
+                                    str(int(movie.runtime) % 60) + " Minutes", wrap = True)
+
+		box = Gtk.Box(margin_bottom = 5, margin_left = 3, margin_right = 3)
+		box.add(self.runtime)
+		self.add(box)
+
+    def update(self, movie):
+        self.runtime.set_label(str(int(movie.runtime) // 60) + " Hours " +
+                                str(int(movie.runtime) % 60) + " Minutes")
+
 
 class DescriptionFrame(Gtk.Frame):
     """Frame for displaying who has seen the movie"""
@@ -52,14 +95,25 @@ class DescriptionFrame(Gtk.Frame):
     def __init__(self, movie):
         Gtk.Frame(self, label_xalign = .1)
 
+        title = Gtk.Label(label = "<b>Description</b>", justify = Gtk.Justification.LEFT,
+                            use_markup = True)
+        self.set_label_widget(title)
+		self.description = Gtk.Label(label = movie.overview, wrap = True)
+
+		box = Gtk.Box(margin_bottom = 5, margin_left = 3, margin_right = 3)
+		box.add(self.description)
+
+		self.add(box)
+
+    def update(self, movie):
+        self.description.set_label(movie.get_viewers_string())
+
 
 class ImageBox(Gtk.Box):
     """Create a box to display an image and movie title"""
 
     def __init__(self, movie):
         Gtk.Box.__init__(self, orientation = Gtk.Orientation.VERTICAL, spacing = 20, margin = 20)
-
-        self.set_size_request(400, -1)
 
         self.title = Gtk.Label(label = movie.get_markup_title(), justify = Gtk.Justification.CENTER,
                             use_markup = True)
@@ -80,6 +134,11 @@ class InfoBox(Gtk.Box):
         Gtk.Box.__init__(self)
 
         self.db = db
+        self.viewFrame = None
+        self.genFrame = None
+        self.ratFrame = None
+        self.runFrame = None
+        self.descFrame = None
 
         self.get_style_context().add_class("linked")
 
