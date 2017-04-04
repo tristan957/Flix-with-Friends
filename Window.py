cb2d3de9584c416c2a03d4dc3af93cb2bf2cd0b9
Tristan Partin <tristan.partin@gmail.com>
2017-04-04 00:14:47 -0500

    Movies added

--- Window.py
+++ Window.py
@@ -3,6 +3,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio, GObject
 
 import random
+
 from Database import Database
 from HeaderBar import HeaderBar
 from SearchBar import SearchBar
