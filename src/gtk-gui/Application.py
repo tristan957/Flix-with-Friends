1ac3002753c9673c525b7604480bd91e2a4eb9e6
Tristan Partin <tristan.partin@gmail.com>
2017-06-30 23:48:24 -0500

    Beginning clean up

--- src/gtk-gui/Application.py
+++ src/gtk-gui/Application.py
@@ -8,56 +8,45 @@ from Window import InitWindow, MainWindow
 
 FLIX_APP_ID = "com.return0software.Flix-with-Friends"
 
+
 class FlixApplication(Gtk.Application):
+    """Acts a program manager"""
 
-	"""
-	Acts a program manager
-	"""
+    initWindow = None
+    mainWindow = None
+    db = None
 
-	def __init__(self):
-		Gtk.Application.__init__(self, application_id = FLIX_APP_ID)
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id=FLIX_APP_ID)
 
-		self.initWindow = None
-		self.mainWindow = None
-		self.db = None
+        self.connect("activate", self.activate_cb)
+        self.connect("shutdown", self.shutdown_cb)
 
-		self.connect("activate", self.activate_cb)
-		self.connect("shutdown", self.shutdown_cb)
+    def windowCheck(self):
+        """Checks if a window is already in use"""
 
-	def windowCheck(self):
+        if self.initWindow is None and self.mainWindow is None:
+            self.initWindow = InitWindow() # create the initial window to get a location
+            self.initWindow.connect("credentials-set", self.createMainWin)
+            self.add_window(self.initWindow)
+            # self.appWindow.connect("delete-event", Gtk.main_quit) # when delete-event signal is received, calls Gtk.main_quit
 
-		"""
-		Checks if a window is already in use
-		"""
+    def activate_cb(self, app):
+        """Starts the program"""
 
-		if self.initWindow == None and self.mainWindow == None:
-			self.initWindow = InitWindow() # create the initial window to get a location
-			self.initWindow.connect("credentials-set", self.createMainWin)
-			self.add_window(self.initWindow)
-			# self.appWindow.connect("delete-event", Gtk.main_quit) # when delete-event signal is received, calls Gtk.main_quit
+        self.windowCheck()
+        self.initWindow.show_all() # display the window and all widgets
 
-	def activate_cb(self, app):
+    def createMainWin(self, win, cred_dict):
+        """Creates the main window"""
 
-		"""
-		Starts the program
-		"""
+        # Database.location = location
+        self.db = Database(cred_dict) # create the database of the given location
+        self.mainWindow = MainWindow(self.db) # create the main window now that we have an initial location
+        self.remove_window(self.initWindow)
+        self.initWindow.destroy()
+        self.add_window(self.mainWindow)
+        self.mainWindow.show_all()
 
-		self.windowCheck()
-		self.initWindow.show_all() # display the window and all widgets
-
-	def createMainWin(self, win, cred_dict):
-
-		"""
-		Creates the main window
-		"""
-
-		# Database.location = location
-		self.db = Database(cred_dict) # create the database of the given location
-		self.mainWindow = MainWindow(self.db) # create the main window now that we have an initial location
-		self.remove_window(self.initWindow)
-		self.initWindow.destroy()
-		self.add_window(self.mainWindow)
-		self.mainWindow.show_all()
-
-	def shutdown_cb(self, app):
-		app.quit() # analogous to self.quit
+    def shutdown_cb(self, app):
+        app.quit() # analogous to self.quit
